b9846d1d48eca8e0f1007c3a035b03bc74554353
hisasann <hisasann25@gmail.com>
2019-02-28 06:33:50 +0900

    chore(pug): add pug and pug-plain-loader of npm modules

--- pages/example/index.vue
+++ pages/example/index.vue
@@ -1,30 +1,19 @@
-<template>
-  <section>
-    <h1>index</h1>
-    <p>
-      <nuxt-link to="/example/ajax/color" no-prefetch>
-        color
-      </nuxt-link>
-    </p>
-    <p>
-      <nuxt-link to="/example/ajax/schema">
-        schema
-      </nuxt-link>
-    </p>
-    <p>
-      <nuxt-link to="/example/c-01/e-02/">
-        /c-01/e-02/
-      </nuxt-link>
-    </p>
-    <p>
-      <n-link to="/example/d-03/f-04/">
-        /d-03/f-04/
-      </n-link>
-    </p>
-    <p>
-      <nuxt-link to="/example/assets-and-static">
-        assets and static
-      </nuxt-link>
-    </p>
-  </section>
+<template lang="pug">
+  section
+    h1 index
+    p
+      nuxt-link(to='/example/ajax/color', no-prefetch)
+        | color
+    p
+      nuxt-link(to='/example/ajax/schema')
+        | schema
+    p
+      nuxt-link(to='/example/c-01/e-02/')
+        | /c-01/e-02/
+    p
+      n-link(to='/example/d-03/f-04/')
+        | /d-03/f-04/
+    p
+      nuxt-link(to='/example/assets-and-static')
+        | assets and static
 </template>
