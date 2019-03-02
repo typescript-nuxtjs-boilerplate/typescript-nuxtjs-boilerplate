0c48549bff63431b0b10399add762fa790ccc4fa
hisasann <hisasann25@gmail.com>
2019-03-02 18:27:50 +0900

    feat: add example/custom-head.vue

---
+++ src/pages/example/custom-head.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  section.container
+    h1.title
+      | custom-head
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+  title: string = ''
+  data() {
+    return {
+      title: 'Hello World!',
+    }
+  }
+
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description',
+        },
+      ],
+    }
+  }
+}
+</script>
