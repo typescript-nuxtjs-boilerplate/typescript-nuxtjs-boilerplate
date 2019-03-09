9daaae96e421866e512fdc0e1c29d7a46092fd4a
hisasann <hisasann25@gmail.com>
2019-03-09 15:42:56 +0900

    feat(404): add error.vue

---
+++ src/layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">
+      ページが見つかりません
+    </h1>
+    <h1 v-else>
+      エラーが発生しました
+    </h1>
+    <nuxt-link to="/example">
+      ホーム
+    </nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    error: undefined
+  }
+}
+</script>
