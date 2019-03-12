819e731fc1a358ddd108a9d541ee50509d74992f
hisasann <hisasann25@gmail.com>
2019-03-12 23:47:35 +0900

    feat(.d.ts): add types/vue-types-vue__$env.d.ts

--- src/plugins/env-inject.ts
+++ src/plugins/env-inject.ts
@@ -1,11 +1,5 @@
 import { Context } from '@nuxt/vue-app'
 
-declare module 'vue/types/vue' {
-  interface Vue {
-    $env: any
-  }
-}
-
 export default (context: Context) => {
   for (const k in context.app.$env) {
     console.log('env-inject:', k, context.app.$env[k])
