adfb11510d1218ac1bf65f3db7f9a0207d1af01c
hisasann <hisasann25@gmail.com>
2019-03-12 23:51:55 +0900

    improve(lazy-load): add height to feel lazy-load

--- src/pages/example/img-lazy-load.vue
+++ src/pages/example/img-lazy-load.vue
@@ -3,17 +3,13 @@
     h1.title
       | assets and static
     div
-      h2 assets lazy-image
+      h2 assets lazy-image lemon-sour.medium.jpg
       <lazy-image src='~assets/images/lemon-sour.medium.jpg' alt='alt'/>
+    div.center
     div
-      h2 assets src
-      img(src='~assets/images/lemon-sour.medium.jpg')
+      h2 assets lazy-image lemon-sour.small.jpg
+      <lazy-image src='~assets/images/lemon-sour.small.jpg' alt='alt'/>
     div
-      h2 assets data-src
-      img(data-src='~assets/images/lemon-sour.small.jpg')
-    div
-      h2 static
-      img(src='/lemon-sour.small.jpg')
     nuxt-link(to='/example/')
       | example top
 </template>
@@ -29,3 +25,9 @@ import LazyImage from '@/components/LazyImage.vue'
 })
 export default class ImgLazyLoad extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.center {
+  height: 1000px;
+}
+</style>
