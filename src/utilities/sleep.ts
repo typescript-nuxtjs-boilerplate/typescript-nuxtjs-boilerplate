a3de28f61d7328313cf8411935c0a0d76d1441a8
hisasann <hisasann25@gmail.com>
2019-03-18 16:16:09 +0900

    lint

--- src/utilities/sleep.ts
+++ src/utilities/sleep.ts
@@ -2,7 +2,7 @@
  * sleep
  * @param {number} milliseconds
  */
-export function sleep(milliseconds: number) {
+export function sleep(milliseconds: number): Promise<void> {
   return new Promise<void>(resolve => {
     setTimeout(() => resolve(), milliseconds)
   })
