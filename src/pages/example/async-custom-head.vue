5e9b068d87634ad0f0d9de1ce7027a3f4a544650
hisasann <hisasann25@gmail.com>
2019-03-29 18:09:45 +0900

    feat: add async custom head

---
+++ src/pages/example/async-custom-head.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  section.container
+    h1.title
+      | custom-head
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+  public title: string = ''
+  public content: string = ''
+
+  public asyncData() {
+    return {
+      content: 'from asyncData'
+    }
+  }
+  public data() {
+    return {
+      title: 'custom-head'
+    }
+  }
+
+  public head() {
+    const robots = {
+      name: 'robots',
+      content: `noindex,nofollow`
+    }
+
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `My custom description ${this.content}`
+        },
+        robots
+      ]
+    }
+  }
+}
+</script>
