f60237d67f54ac8ab8be96d56423729fc64fc896
hisasann <hisasann25@gmail.com>
2019-04-09 17:32:51 +0900

    feat(constants): update api.ts

--- src/common/constants/api.ts
+++ src/common/constants/api.ts
@@ -2,6 +2,7 @@ import Env from '@/common/env/'
 
 const getBffUrl = (path: string): string => [Env.url, path].join('')
 
+/** API のエンドポイント */
 export const API_ENDPOINT = {
   /** ログイン状態チェック */
   LOGIN_CHECK: getBffUrl('/login-check'),
@@ -9,4 +10,15 @@ export const API_ENDPOINT = {
   LOGIN: getBffUrl('/login'),
   /** ログアウト */
   LOGOUT: getBffUrl('/logout')
+}
+
+/** アクセストークンヘッダ名 - API サーバーとのログインセッション用のトークン名 */
+export const ACCESS_TOKEN_NAME = 'access-token'
+
+/** HTTP ステータス */
+export const HTTP_STATUS = {
+  /** OK */
+  OK: 200,
+  /** 認証が必要である */
+  UNAUTHORIZED: 401
 }
