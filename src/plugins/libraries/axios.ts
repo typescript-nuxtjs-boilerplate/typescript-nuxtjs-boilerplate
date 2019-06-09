1af828071aad110b12b913a4ad137d73d5e79067
hisasann <hisasann25@gmail.com>
2019-06-10 06:48:08 +0900

    feat: add comment to axios.ts

--- src/plugins/libraries/axios.ts
+++ src/plugins/libraries/axios.ts
@@ -7,6 +7,9 @@ import Vue from 'vue'
 import { setToken, unsetToken, getTokenFromCookie } from '@/utilities/'
 
 export default ({ $axios, app, req, error }): void => {
+  /**
+   * $axios.onRequest
+   */
   $axios.onRequest(
     (config: AxiosRequestConfig): void => {
       const token = getTokenFromCookie(req)
@@ -19,6 +22,9 @@ export default ({ $axios, app, req, error }): void => {
     }
   )
 
+  /**
+   * $axios.onResponse
+   */
   $axios.onResponse(
     (response): void => {
       const token = response.headers[app.$C.ACCESS_TOKEN_NAME]
@@ -35,6 +41,9 @@ export default ({ $axios, app, req, error }): void => {
     }
   )
 
+  /**
+   * $axios.onResponseError
+   */
   $axios.onResponseError(
     (response: AxiosError): void => {
       console.log('$axios.onResponseError')
