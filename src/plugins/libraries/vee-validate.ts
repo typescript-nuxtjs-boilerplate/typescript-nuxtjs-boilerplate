8df6b003493e44570f2d5d9e20e8180a30757824
hisamatsu yoshiyuki <hisasann25@gmail.com>
2019-08-03 06:16:24 +0900

    🍺 chore(vee-validata): add custom message

--- src/plugins/libraries/vee-validate.ts
+++ src/plugins/libraries/vee-validate.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VeeValidate, { Validator } from 'vee-validate'
+import _ from 'lodash'
 import ja from 'vee-validate/dist/locale/ja'
 import * as customRules from './validator/rules/'
 
@@ -7,7 +8,14 @@ import * as customRules from './validator/rules/'
 // Component Injections | VeeValidate - https://baianat.github.io/vee-validate/concepts/injections.html#disabling-automatic-injection
 Vue.use(VeeValidate, { inject: false })
 
-Validator.localize('ja', ja)
+const jaCustomDict = {
+  messages: {
+    email: (w): string => `${w}のメールアドレスはダメです`,
+    required: (w): string => `${w}は必須です`
+  }
+}
+const mergedDict = _.merge(ja, jaCustomDict)
+Validator.localize('ja', mergedDict)
 
 for (const name in customRules) {
   Validator.extend(name, customRules[name])
