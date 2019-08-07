5ef3ab1459e798a30b0a2abf72f271286fed00a1
hisamatsu yoshiyuki <hisasann25@gmail.com>
2019-08-07 16:51:02 +0900

    🥗 refactor: remove unnecessary env

--- src/common/env/index.ts
+++ src/common/env/index.ts
@@ -1,17 +1,13 @@
 import EnvLocal from './env.local'
 import { IEnv } from '@/interfaces/app/IEnv'
 
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
-console.log('process.env.BUILD_ENV: ', process.env.BUILD_ENV)
-
 let Env: IEnv
 if (process.env.BUILD_ENV === 'docker') {
   /** docker のビルド環境の環境変数から値を取ってくる */
   Env = {
     envName: process.env.envName,
     internalEndpointUrl: process.env.internalEndpointUrl,
-    externalEndpointUrl: process.env.externalEndpointUrl,
-    gtmContainerId: process.env.gtmContainerId
+    externalEndpointUrl: process.env.externalEndpointUrl
   } as IEnv
 } else {
   /** docker でビルドされていない場合は、 .env.local から値を取ってくる */
