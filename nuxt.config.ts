0dce645f5335fe5614dbb3e39938fef157ead726
hisamatsu yoshiyuki <hisasann25@gmail.com>
2019-11-21 14:33:18 +0900

    🍝 chore(nuxt.config.ts): add viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,10 +118,14 @@ const config: NuxtConfiguration = {
    * Headers of the page
    */
   head: {
-    title: pkg.name,
+    titleTemplate: `%s - ${pkg.name}`,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'viewport',
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
